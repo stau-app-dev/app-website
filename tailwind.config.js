9bffdac8c478bf1339f760f8100ca8a76ed15dbf
Martin Sit <46298951+martin-sit@users.noreply.github.com>
2022-04-20 09:58:52 -0400

    build: tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        primary: '#8d122f',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
