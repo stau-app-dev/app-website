d3dbe17b5bc789c5215ee6be5d5430607f6f69d4
Martin <46298951+martin226@users.noreply.github.com>
2022-04-22 09:29:19 -0400

    feat: footer component (#9)

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,7 +23,7 @@ export default {
       },
       { name: 'format-detection', content: 'telephone=no' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: 'favicon.ico' }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
