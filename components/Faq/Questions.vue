ebb2f61e1f29868767f054d819152091fd215f13
Martin Sit <46298951+martin226@users.noreply.github.com>
2022-04-22 17:19:36 -0400

    feat: design changes to FAQ

--- components/Faq/Questions.vue
+++ components/Faq/Questions.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="px-4 md:px-16 lg:px-20 py-16">
-    <h1 class="text-4xl font-bold text-center">
+    <h1 class="font-serif text-4xl md:text-5xl font-bold text-center">
       Frequently <span class="text-primary">Asked</span> Questions
     </h1>
     <div class="flex justify-center">
-      <hr class="border-b-4 border-solid w-8/12 my-12 border-gray-500" />
+      <hr class="border-b-4 border-solid w-8/12 my-12 border-primary" />
     </div>
     <FaqQuestion
       v-for="(question, index) in questions"
