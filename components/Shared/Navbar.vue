e8085db9f5ea4483e11aad18439cf3aecd05479b
Martin <46298951+martin226@users.noreply.github.com>
2022-04-22 08:44:15 -0400

    feat: change navbar links, remove scroll (#7)

--- components/Shared/Navbar.vue
+++ components/Shared/Navbar.vue
@@ -32,13 +32,12 @@
       :class="{ block: isOpen, hidden: !isOpen }"
     >
       <div class="text-base lg:flex-grow lg:text-right lg:mr-3">
-        <a
-          href="#features"
+        <NuxtLink
+          to="/faq"
           class="cursor-pointer block mt-4 lg:inline-block lg:mt-0 text-gray-600 hover:text-primary mr-4"
-          @click="$emit('scroll-to', 'features')"
         >
-          Features
-        </a>
+          FAQ
+        </NuxtLink>
         <NuxtLink
           to="/team"
           class="block mt-4 lg:inline-block lg:mt-0 text-gray-600 hover:text-primary mr-4"
@@ -47,19 +46,19 @@
         </NuxtLink>
       </div>
       <div class="lg:ml-3">
-        <a
-          href="#cta"
+        <NuxtLink
+          to="/"
           class="cursor-pointer font-semibold inline-block text-sm px-5 py-2.5 leading-none border rounded-2xl bg-primary text-white hover:opacity-75 transition-opacity duration-150 mt-4 lg:mt-0"
-          @click="$emit('scroll-to', 'cta')"
-          >Download</a
         >
+          Download
+        </NuxtLink>
       </div>
     </div>
   </nav>
 </template>
 <script>
 export default {
-  name: 'LandingNavbar',
+  name: 'SharedNavbar',
   data() {
     return {
       isOpen: false,
--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,12 @@
 <template>
   <div>
     <div class="min-h-screen flex flex-col">
-      <LandingNavbar
-        ref="nav"
-        class="w-full fixed top-0"
-        @scroll-to="scrollTo($event)"
-      />
-      <LandingHero ref="hero" class="flex-1 pt-36" />
+      <SharedNavbar class="w-full fixed top-0" />
+      <LandingHero class="flex-1 pt-36" />
     </div>
-    <LandingQuote ref="quote" />
-    <LandingFeatures ref="features" />
-    <LandingAdditionalInfo ref="additionalinfo" />
+    <LandingQuote />
+    <LandingFeatures />
+    <LandingAdditionalInfo />
   </div>
 </template>
 
@@ -19,20 +15,5 @@ import Vue from 'vue';
 
 export default Vue.extend({
   name: 'IndexPage',
-  mounted() {
-    // Auto scroll to anchor if there is one
-    const hash = window.location.hash;
-    if (hash) {
-      this.scrollTo(hash.substr(1));
-    }
-  },
-  methods: {
-    scrollTo(ref: string) {
-      const el = (this.$refs[ref] as Vue).$el;
-      if (el) {
-        el.scrollIntoView({ behavior: 'smooth' });
-      }
-    },
-  },
 });
 </script>
