c0ace70384fb7c4b2e5cd8a2140cf6b95a98a5ff
Martin <46298951+martin226@users.noreply.github.com>
2022-04-23 13:01:03 -0400

    refactor: separate data from components, add types (#13)
    * refactor: separate data from Questions component
    
    * refactor: separate data from Grid component
    
    * feat: question type
    
    * feat: team type

--- components/Team/Grid.vue
+++ components/Team/Grid.vue
@@ -7,7 +7,7 @@
       <hr class="border-b-4 border-solid w-8/12 my-12 border-primary" />
     </div>
     <TeamCard
-      v-for="(group, index) in team"
+      v-for="(group, index) in teams"
       :key="`group-${index}`"
       :group="group"
     />
@@ -15,128 +15,15 @@
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
+import Vue, { PropType } from 'vue';
+import Team from '@/types/team';
 export default Vue.extend({
   name: 'TeamGrid',
-  data() {
-    return {
-      team: [
-        {
-          year: '2021-2022',
-          members: [
-            {
-              name: 'John Doe',
-              position: 'Cool Position',
-              image: 'https://via.placeholder.com/800',
-              bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-            },
-            {
-              name: 'John Doe',
-              position: 'Cool Position',
-              image: 'https://via.placeholder.com/800',
-              bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-            },
-            {
-              name: 'John Doe',
-              position: 'Cool Position',
-              image: 'https://via.placeholder.com/800',
-              bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-            },
-            {
-              name: 'John Doe',
-              position: 'Cool Position',
-              image: 'https://via.placeholder.com/800',
-              bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-            },
-            {
-              name: 'John Doe',
-              position: 'Cool Position',
-              image: 'https://via.placeholder.com/800',
-              bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-            },
-            {
-              name: 'John Doe',
-              position: 'Cool Position',
-              image: 'https://via.placeholder.com/800',
-              bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-            },
-            {
-              name: 'John Doe',
-              position: 'Cool Position',
-              image: 'https://via.placeholder.com/800',
-              bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-            },
-            {
-              name: 'John Doe',
-              position: 'Cool Position',
-              image: 'https://via.placeholder.com/800',
-              bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-            },
-          ],
-        },
-        {
-          year: '2020-2021',
-          members: [
-            {
-              name: 'John Doe',
-              position: 'Cool Position',
-              image: 'https://via.placeholder.com/800',
-              bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-            },
-            {
-              name: 'John Doe',
-              position: 'Cool Position',
-              image: 'https://via.placeholder.com/800',
-              bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-            },
-            {
-              name: 'John Doe',
-              position: 'Cool Position',
-              image: 'https://via.placeholder.com/800',
-              bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-            },
-            {
-              name: 'John Doe',
-              position: 'Cool Position',
-              image: 'https://via.placeholder.com/800',
-              bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-            },
-            {
-              name: 'John Doe',
-              position: 'Cool Position',
-              image: 'https://via.placeholder.com/800',
-              bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-            },
-            {
-              name: 'John Doe',
-              position: 'Cool Position',
-              image: 'https://via.placeholder.com/800',
-              bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-            },
-            {
-              name: 'John Doe',
-              position: 'Cool Position',
-              image: 'https://via.placeholder.com/800',
-              bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-            },
-            {
-              name: 'John Doe',
-              position: 'Cool Position',
-              image: 'https://via.placeholder.com/800',
-              bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-            },
-          ],
-        },
-      ] as {
-        year: string;
-        members: {
-          name: string;
-          position: string;
-          image: string;
-          bio: string;
-        }[];
-      }[],
-    };
+  props: {
+    teams: {
+      type: Array as PropType<Team[]>,
+      required: true,
+    },
   },
 });
 </script>
--- pages/team.vue
+++ pages/team.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="min-h-screen flex flex-col">
       <SharedNavbar class="w-full fixed top-0" />
-      <TeamGrid class="pt-36" />
+      <TeamGrid class="pt-36" :teams="teams" />
     </div>
     <SharedFooter />
   </div>
@@ -10,8 +10,121 @@
 
 <script lang="ts">
 import Vue from 'vue';
+import Team from '@/types/team';
 
 export default Vue.extend({
   name: 'TeamPage',
+  data() {
+    return {
+      teams: [
+        {
+          year: '2021-2022',
+          members: [
+            {
+              name: 'John Doe',
+              position: 'Cool Position',
+              image: 'https://via.placeholder.com/800',
+              bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+            },
+            {
+              name: 'John Doe',
+              position: 'Cool Position',
+              image: 'https://via.placeholder.com/800',
+              bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+            },
+            {
+              name: 'John Doe',
+              position: 'Cool Position',
+              image: 'https://via.placeholder.com/800',
+              bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+            },
+            {
+              name: 'John Doe',
+              position: 'Cool Position',
+              image: 'https://via.placeholder.com/800',
+              bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+            },
+            {
+              name: 'John Doe',
+              position: 'Cool Position',
+              image: 'https://via.placeholder.com/800',
+              bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+            },
+            {
+              name: 'John Doe',
+              position: 'Cool Position',
+              image: 'https://via.placeholder.com/800',
+              bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+            },
+            {
+              name: 'John Doe',
+              position: 'Cool Position',
+              image: 'https://via.placeholder.com/800',
+              bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+            },
+            {
+              name: 'John Doe',
+              position: 'Cool Position',
+              image: 'https://via.placeholder.com/800',
+              bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+            },
+          ],
+        },
+        {
+          year: '2020-2021',
+          members: [
+            {
+              name: 'John Doe',
+              position: 'Cool Position',
+              image: 'https://via.placeholder.com/800',
+              bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+            },
+            {
+              name: 'John Doe',
+              position: 'Cool Position',
+              image: 'https://via.placeholder.com/800',
+              bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+            },
+            {
+              name: 'John Doe',
+              position: 'Cool Position',
+              image: 'https://via.placeholder.com/800',
+              bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+            },
+            {
+              name: 'John Doe',
+              position: 'Cool Position',
+              image: 'https://via.placeholder.com/800',
+              bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+            },
+            {
+              name: 'John Doe',
+              position: 'Cool Position',
+              image: 'https://via.placeholder.com/800',
+              bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+            },
+            {
+              name: 'John Doe',
+              position: 'Cool Position',
+              image: 'https://via.placeholder.com/800',
+              bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+            },
+            {
+              name: 'John Doe',
+              position: 'Cool Position',
+              image: 'https://via.placeholder.com/800',
+              bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+            },
+            {
+              name: 'John Doe',
+              position: 'Cool Position',
+              image: 'https://via.placeholder.com/800',
+              bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+            },
+          ],
+        },
+      ] as Array<Team>,
+    };
+  },
 });
 </script>
