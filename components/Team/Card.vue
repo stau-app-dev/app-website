b2778469666a401fe17e45b000e47a987164c2c7
Martin Sit <46298951+martin226@users.noreply.github.com>
2022-04-23 10:45:04 -0400

    feat: team card component

---
+++ components/Team/Card.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="text-center bg-gray-50 p-8 rounded-lg mb-16">
+    <h3 class="text-3xl font-semibold mb-8 font-serif">{{ group.year }}</h3>
+    <div class="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4">
+      <div
+        v-for="(member, index) in group.members"
+        :key="`member-${index}`"
+        class="p-8 text-center flex flex-col justify-center gap-y-3"
+      >
+        <img :src="member.image" :alt="member.name" class="rounded-lg" />
+        <h4 class="text-xl font-semibold">{{ member.name }}</h4>
+        <p class="text-sm text-gray-600">{{ member.position }}</p>
+        <p>{{ member.bio }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+export default Vue.extend({
+  name: 'TeamCard',
+  props: {
+    group: {
+      type: Object,
+      required: true,
+    },
+  },
+});
+</script>
