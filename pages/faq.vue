ef2ec39427ffed2882db01e9826eb1da6923f254
Martin Sit <46298951+martin226@users.noreply.github.com>
2022-04-23 12:43:23 -0400

    refactor: separate data from Questions component

--- pages/faq.vue
+++ pages/faq.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="min-h-screen flex flex-col">
       <SharedNavbar class="w-full fixed top-0" />
-      <FaqQuestions class="pt-36" />
+      <FaqQuestions class="pt-36" :questions="questions" />
     </div>
     <SharedFooter />
   </div>
@@ -13,5 +13,47 @@ import Vue from 'vue';
 
 export default Vue.extend({
   name: 'FaqPage',
+  data() {
+    return {
+      questions: [
+        {
+          isOpen: true,
+          question: 'Lorem ipsum dolor sit amet?',
+          answer:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consectetur libero id faucibus nisl tincidunt eget. Ullamcorper sit amet risus nullam. Viverra justo nec ultrices dui sapien eget mi proin. Commodo odio aenean sed adipiscing diam donec adipiscing tristique. Morbi tincidunt ornare massa eget egestas. Nisl condimentum id venenatis a condimentum vitae. Pretium nibh ipsum consequat nisl. Cursus mattis molestie a iaculis at erat. Lectus magna fringilla urna porttitor rhoncus dolor. Posuere morbi leo urna molestie at elementum eu facilisis sed. Habitasse platea dictumst quisque sagittis purus sit amet volutpat. Vitae suscipit tellus mauris a diam. Consectetur adipiscing elit ut aliquam purus sit amet.\nSollicitudin tempor id eu nisl. Neque volutpat ac tincidunt vitae semper quis lectus nulla. Vivamus arcu felis bibendum ut tristique et egestas. Etiam sit amet nisl purus in mollis. Aliquet bibendum enim facilisis gravida neque convallis a cras. Fermentum et sollicitudin ac orci phasellus. Odio aenean sed adipiscing diam donec. Bibendum ut tristique et egestas quis ipsum. Sed cras ornare arcu dui vivamus arcu felis. Porttitor lacus luctus accumsan tortor posuere ac ut.',
+        },
+        {
+          isOpen: false,
+          question: 'Lorem ipsum dolor sit amet?',
+          answer:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consectetur libero id faucibus nisl tincidunt eget. Ullamcorper sit amet risus nullam. Viverra justo nec ultrices dui sapien eget mi proin. Commodo odio aenean sed adipiscing diam donec adipiscing tristique. Morbi tincidunt ornare massa eget egestas. Nisl condimentum id venenatis a condimentum vitae. Pretium nibh ipsum consequat nisl. Cursus mattis molestie a iaculis at erat. Lectus magna fringilla urna porttitor rhoncus dolor. Posuere morbi leo urna molestie at elementum eu facilisis sed. Habitasse platea dictumst quisque sagittis purus sit amet volutpat. Vitae suscipit tellus mauris a diam. Consectetur adipiscing elit ut aliquam purus sit amet.\nSollicitudin tempor id eu nisl. Neque volutpat ac tincidunt vitae semper quis lectus nulla. Vivamus arcu felis bibendum ut tristique et egestas. Etiam sit amet nisl purus in mollis. Aliquet bibendum enim facilisis gravida neque convallis a cras. Fermentum et sollicitudin ac orci phasellus. Odio aenean sed adipiscing diam donec. Bibendum ut tristique et egestas quis ipsum. Sed cras ornare arcu dui vivamus arcu felis. Porttitor lacus luctus accumsan tortor posuere ac ut.',
+        },
+        {
+          isOpen: false,
+          question: 'Lorem ipsum dolor sit amet?',
+          answer:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consectetur libero id faucibus nisl tincidunt eget. Ullamcorper sit amet risus nullam. Viverra justo nec ultrices dui sapien eget mi proin. Commodo odio aenean sed adipiscing diam donec adipiscing tristique. Morbi tincidunt ornare massa eget egestas. Nisl condimentum id venenatis a condimentum vitae. Pretium nibh ipsum consequat nisl. Cursus mattis molestie a iaculis at erat. Lectus magna fringilla urna porttitor rhoncus dolor. Posuere morbi leo urna molestie at elementum eu facilisis sed. Habitasse platea dictumst quisque sagittis purus sit amet volutpat. Vitae suscipit tellus mauris a diam. Consectetur adipiscing elit ut aliquam purus sit amet.\nSollicitudin tempor id eu nisl. Neque volutpat ac tincidunt vitae semper quis lectus nulla. Vivamus arcu felis bibendum ut tristique et egestas. Etiam sit amet nisl purus in mollis. Aliquet bibendum enim facilisis gravida neque convallis a cras. Fermentum et sollicitudin ac orci phasellus. Odio aenean sed adipiscing diam donec. Bibendum ut tristique et egestas quis ipsum. Sed cras ornare arcu dui vivamus arcu felis. Porttitor lacus luctus accumsan tortor posuere ac ut.',
+        },
+        {
+          isOpen: false,
+          question: 'Lorem ipsum dolor sit amet?',
+          answer:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consectetur libero id faucibus nisl tincidunt eget. Ullamcorper sit amet risus nullam. Viverra justo nec ultrices dui sapien eget mi proin. Commodo odio aenean sed adipiscing diam donec adipiscing tristique. Morbi tincidunt ornare massa eget egestas. Nisl condimentum id venenatis a condimentum vitae. Pretium nibh ipsum consequat nisl. Cursus mattis molestie a iaculis at erat. Lectus magna fringilla urna porttitor rhoncus dolor. Posuere morbi leo urna molestie at elementum eu facilisis sed. Habitasse platea dictumst quisque sagittis purus sit amet volutpat. Vitae suscipit tellus mauris a diam. Consectetur adipiscing elit ut aliquam purus sit amet.\nSollicitudin tempor id eu nisl. Neque volutpat ac tincidunt vitae semper quis lectus nulla. Vivamus arcu felis bibendum ut tristique et egestas. Etiam sit amet nisl purus in mollis. Aliquet bibendum enim facilisis gravida neque convallis a cras. Fermentum et sollicitudin ac orci phasellus. Odio aenean sed adipiscing diam donec. Bibendum ut tristique et egestas quis ipsum. Sed cras ornare arcu dui vivamus arcu felis. Porttitor lacus luctus accumsan tortor posuere ac ut.',
+        },
+        {
+          isOpen: false,
+          question: 'Lorem ipsum dolor sit amet?',
+          answer:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consectetur libero id faucibus nisl tincidunt eget. Ullamcorper sit amet risus nullam. Viverra justo nec ultrices dui sapien eget mi proin. Commodo odio aenean sed adipiscing diam donec adipiscing tristique. Morbi tincidunt ornare massa eget egestas. Nisl condimentum id venenatis a condimentum vitae. Pretium nibh ipsum consequat nisl. Cursus mattis molestie a iaculis at erat. Lectus magna fringilla urna porttitor rhoncus dolor. Posuere morbi leo urna molestie at elementum eu facilisis sed. Habitasse platea dictumst quisque sagittis purus sit amet volutpat. Vitae suscipit tellus mauris a diam. Consectetur adipiscing elit ut aliquam purus sit amet.\nSollicitudin tempor id eu nisl. Neque volutpat ac tincidunt vitae semper quis lectus nulla. Vivamus arcu felis bibendum ut tristique et egestas. Etiam sit amet nisl purus in mollis. Aliquet bibendum enim facilisis gravida neque convallis a cras. Fermentum et sollicitudin ac orci phasellus. Odio aenean sed adipiscing diam donec. Bibendum ut tristique et egestas quis ipsum. Sed cras ornare arcu dui vivamus arcu felis. Porttitor lacus luctus accumsan tortor posuere ac ut.',
+        },
+        {
+          isOpen: false,
+          question: 'Lorem ipsum dolor sit amet?',
+          answer:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consectetur libero id faucibus nisl tincidunt eget. Ullamcorper sit amet risus nullam. Viverra justo nec ultrices dui sapien eget mi proin. Commodo odio aenean sed adipiscing diam donec adipiscing tristique. Morbi tincidunt ornare massa eget egestas. Nisl condimentum id venenatis a condimentum vitae. Pretium nibh ipsum consequat nisl. Cursus mattis molestie a iaculis at erat. Lectus magna fringilla urna porttitor rhoncus dolor. Posuere morbi leo urna molestie at elementum eu facilisis sed. Habitasse platea dictumst quisque sagittis purus sit amet volutpat. Vitae suscipit tellus mauris a diam. Consectetur adipiscing elit ut aliquam purus sit amet.\nSollicitudin tempor id eu nisl. Neque volutpat ac tincidunt vitae semper quis lectus nulla. Vivamus arcu felis bibendum ut tristique et egestas. Etiam sit amet nisl purus in mollis. Aliquet bibendum enim facilisis gravida neque convallis a cras. Fermentum et sollicitudin ac orci phasellus. Odio aenean sed adipiscing diam donec. Bibendum ut tristique et egestas quis ipsum. Sed cras ornare arcu dui vivamus arcu felis. Porttitor lacus luctus accumsan tortor posuere ac ut.',
+        },
+      ],
+    };
+  },
 });
 </script>
