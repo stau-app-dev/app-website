a9f082629900ad3f7859f1be296fcd661e96cdf2
kmiu2 <kennymiu@rogers.com>
2022-04-25 19:04:00 -0400

    feat: working images

--- components/Team/Card.vue
+++ components/Team/Card.vue
@@ -7,7 +7,11 @@
         :key="`member-${index}`"
         class="p-8 text-center flex flex-col gap-y-3"
       >
-        <img :src="member.image" :alt="member.name" class="rounded-lg" />
+        <img
+          :src="require(`~/assets/img/team/${member.image}`)"
+          :alt="member.name"
+          class="rounded-lg"
+        />
         <h4 class="text-xl font-semibold">{{ member.name }}</h4>
         <p class="text-sm text-gray-600">{{ member.position }}</p>
         <p>{{ member.bio }}</p>
