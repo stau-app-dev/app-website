56b0ecad52a42c28bba5bfe3f19668d37f8951fc
Martin Sit <46298951+martin226@users.noreply.github.com>
2022-04-25 22:49:18 -0400

    feat: error component

---
+++ components/Login/Errors.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div class="bg-primary text-white font-bold rounded-t px-4 py-2">Error</div>
+    <div
+      class="border border-t-0 border-red-600 rounded-b bg-red-100 px-4 py-3 text-red-700"
+    >
+      <ul>
+        <li v-for="(error, index) in errors" :key="`error-${index}`">
+          {{ error }}
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'LoginErrors',
+  props: {
+    errors: {
+      type: Array,
+      required: true,
+    },
+  },
+});
+</script>
