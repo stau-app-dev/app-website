198689148e2a82ad0c6d621760630e1b1178f1fc
Martin Sit <46298951+martin226@users.noreply.github.com>
2022-04-25 23:47:13 -0400

    feat: login page

--- components/Login/Form.vue
+++ components/Login/Form.vue
@@ -1,76 +1,70 @@
 <template>
-  <div class="flex flex-col h-screen">
-    <div class="container mx-auto px-4 h-full">
-      <div class="flex content-center items-center justify-center h-full">
-        <div class="w-full lg:w-4/12 px-4">
-          <div
-            class="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-gray-300 border-0"
-          >
-            <div class="flex-auto px-4 lg:px-10 py-10">
-              <form ref="login" @submit.prevent="login">
-                <div class="relative w-full mb-3">
-                  <label
-                    class="block uppercase text-gray-700 text-xs font-bold mb-2"
-                    for="grid-password"
-                    >Email</label
-                  ><input
-                    v-model="email"
-                    type="email"
-                    class="border-0 px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full"
-                    placeholder="Email"
-                    style="transition: all 0.15s ease 0s"
-                  />
-                </div>
-                <div class="relative w-full mb-3">
-                  <label
-                    class="block uppercase text-gray-700 text-xs font-bold mb-2"
-                    for="grid-password"
-                    >Password</label
-                  ><input
-                    v-model="password"
-                    type="password"
-                    class="border-0 px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full"
-                    placeholder="Password"
-                    style="transition: all 0.15s ease 0s"
-                  />
-                </div>
-                <div class="text-center mt-6">
-                  <button
-                    class="bg-gray-900 text-white active:bg-gray-700 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full"
-                    type="submit"
-                    style="transition: all 0.15s ease 0s"
-                  >
-                    Sign In
-                  </button>
-                </div>
-              </form>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+  <div>
+    <form class="flex flex-col space-y-4" @submit.prevent="login">
+      <LoginErrors v-if="getErrors.length" :errors="getErrors" />
+      <input
+        v-model="username"
+        class="py-4 px-3 bg-transparent rounded-xl text-gray-400 border-gray-300 shadow-sm focus:text-gray-500 focus:border-gray-700 focus:ring-0 transition-colors duration-150"
+        type="text"
+        placeholder="Username"
+      />
+      <input
+        v-model="password"
+        class="py-4 px-3 bg-transparent rounded-xl text-gray-400 border-gray-300 shadow-sm focus:text-gray-500 focus:border-gray-700 focus:ring-0 transition-colors duration-150"
+        type="password"
+        placeholder="Password"
+      />
+      <button
+        class="py-4 font-semibold text-white bg-primary hover:opacity-90 rounded-xl transition-opacity duration-150 shadow-2xl"
+        type="submit"
+      >
+        Login
+      </button>
+    </form>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
+import { mapState } from 'vuex';
 export default Vue.extend({
   name: 'LoginForm',
 
   data() {
     return {
-      email: '',
+      username: '',
       password: '',
     };
   },
-
+  computed: {
+    ...mapState({
+      getErrors: 'errors',
+    }),
+  },
   methods: {
-    login(e: Event) {
-      e.preventDefault();
-      if (this.email === '' || this.password === '') {
-        alert('Please fill in all fields');
-      } else {
-        alert('You are now logged in');
+    async login() {
+      this.$store.commit('clearErrors');
+      if (!this.username || !this.password) {
+        this.$store.commit('addError', 'Username and password are required');
+        return;
+      }
+      try {
+        await this.$fire.auth.signInWithEmailAndPassword(
+          this.username.includes('@')
+            ? this.username
+            : this.username + '@stachs.com',
+          this.password
+        );
+      } catch (e: any) {
+        if (e.code === 'auth/user-not-found') {
+          this.$store.commit('addError', 'User not found');
+        } else if (e.code === 'auth/wrong-password') {
+          this.$store.commit('addError', 'Wrong password');
+        } else if (e.code === 'auth/invalid-email') {
+          this.$store.commit('addError', 'Invalid email');
+        } else {
+          this.$store.commit('addError', e.message);
+        }
       }
     },
   },
--- pages/login.vue
+++ pages/login.vue
@@ -2,7 +2,19 @@
   <div>
     <div class="min-h-screen flex flex-col">
       <SharedNavbar class="w-full fixed top-0" />
-      <LoginForm />
+      <div class="pt-36">
+        <div class="px-4 md:px-16 lg:px-20 py-16">
+          <h1 class="font-serif text-4xl md:text-5xl font-bold text-center">
+            Staff <span class="text-primary">Login</span>
+          </h1>
+          <div class="flex justify-center">
+            <hr class="border-b-4 border-solid w-8/12 my-12 border-primary" />
+          </div>
+          <div class="flex justify-center">
+            <LoginForm class="w-full sm:w-6/12" />
+          </div>
+        </div>
+      </div>
     </div>
     <SharedFooter />
   </div>
@@ -13,5 +25,6 @@ import Vue from 'vue';
 
 export default Vue.extend({
   name: 'LoginPage',
+  middleware: 'auth',
 });
 </script>
