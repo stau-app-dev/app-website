2d50024b23b044481afb0454d497e66c4b0656f0
Alfred Cadawas <alfred.cadawas@gmail.com>
2025-09-25 12:01:18 -0400

    removed old mockup

--- components/Landing/Features.vue
+++ components/Landing/Features.vue
@@ -1,19 +1,6 @@
 <template>
-  <div class="grid grid-cols-1 lg:grid-cols-3 lg:px-16 pt-16 pb-24 gap-16">
-    <div class="hidden lg:flex flex-col justify-center items-center">
-      <div
-        class="rounded-full bg-primary w-72 h-72 md:w-80 md:h-80 lg:w-96 lg:h-96 flex justify-center items-center"
-      >
-        <img
-          src="~/assets/img/iphone-mockup-2.png"
-          alt="iPhone Mockup"
-          class="-mt-8 ml-36 transform scale-75"
-        />
-      </div>
-    </div>
-    <div
-      class="grid grid-cols-1 lg:grid-cols-2 lg:grid-rows-2 px-8 md:px-16 lg:px-20 lg:col-span-2 gap-y-8 lg:pt-12"
-    >
+  <div class="px-8 md:px-16 lg:px-32 pt-16 pb-24">
+    <div class="grid grid-cols-1 sm:grid-cols-2 max-w-4xl mx-auto gap-y-12 gap-x-12">
       <div class="flex flex-col items-center">
         <img src="~/assets/img/icons/gift.svg" alt="Gift Icon" class="h-16" />
         <h3 class="text-2xl font-bold text-center">Free Forever</h3>
